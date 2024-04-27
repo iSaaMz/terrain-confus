e8dece70dcd99be7c0fda78cc8bf0b157483635f
MoussaST <moussa21sr@gmail.com>
2024-04-27 15:32:18 +0200

    Ajout Template

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import Service1 from "./components/Service1.vue";
+import Service2 from "./components/Service2.vue";
+import Feature from "./components/Feature.vue";
+import Pricing from "./components/Pricing.vue";
+import Advantage from "./components/Advantage.vue";
+import Testimonial from "./components/Testimonial.vue";
+import About from "./components/About.vue";
+import Blog from "./components/Blog.vue";
+import Contact from "./components/Contact.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Header />
+  <Hero />
+  <Service1 />
+  <Service2 />
+  <Feature />
+  <Pricing />
+  <Pricing />
+  <Advantage />
+  <Testimonial />
+  <About />
+  <Blog />
+  <Contact />
+  <Footer />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>

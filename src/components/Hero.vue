6e53dbfc8125729ca5e03dab504ad254b1d6825e
iSaaMz <sami.assiakh@gmail.com>
2024-05-22 09:01:27 +0200

    update hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,9 +2,9 @@
 import mainPhoto from "../assets/images/hero.svg";
 import { themeColor } from "../data/items";
 
-const heroHeading = "Promote Your Products by Colorlib";
-const buttonSeeFeature = "See Features";
-const buttonPricing = "Pricing";
+const heroHeading = "Découvrez les aventures en Terrain Confus !";
+const buttonSeeFeature = "Découvrir";
+const buttonPricing = "Rejoindre";
 </script>
 
 <template>
@@ -14,11 +14,7 @@ const buttonPricing = "Pricing";
         <div class="col-12">
           <div class="dots"></div>
           <div class="row align-items-center">
-            <div
-              class="col-lg-7 ml-auto order-lg-2"
-              data-aos="fade-right"
-              data-aos-delay="400"
-            >
+            <div class="col-lg-7 ml-auto order-lg-2" data-aos="fade-right" data-aos-delay="400">
               <img :src="mainPhoto" alt="Image" class="img-fluid" />
             </div>
             <div class="col-lg-5">
@@ -27,36 +23,17 @@ const buttonPricing = "Pricing";
               </h1>
               <div class="excerpt" data-aos="fade-up" data-aos-delay="100">
                 <p>
-                  Another cool free html css template by
-                  <span
-                    style="
-                      background-color: rgba(64, 123, 255, 0.07);
-                      color: #407bff;
-                    "
-                    >Colorlib</span
-                  >
-                  Far far away, behind the word mountains, far from the
-                  countries Vokalia and Consonantia, there live the blind texts.
-                  Separated they live in Bookmarksgrove right at the coast
+                  Découvrez des récits captivants, des conseils pratiques et des aventures en terrains difficiles et
+                  mystérieux. Plongez dans l'inconnu et explorez les recoins les plus énigmatiques de la nature.
                 </p>
               </div>
               <p data-aos="fade-up" data-aos-delay="200">
-                <a
-                  href="#features-section"
-                  class="btn btn-primary smoothscroll"
-                  style="margin-right: 5px"
-                  :style="[
-                    { background: themeColor },
-                    { borderColor: themeColor },
-                  ]"
-                  >{{ buttonSeeFeature }}</a
-                >
-                <a
-                  href="#pricing-section"
-                  class="btn btn-outline-primary smoothscroll pricing"
-                  :style="[{ color: themeColor }, { borderColor: themeColor }]"
-                  >{{ buttonPricing }}</a
-                >
+                <a href="#features-section" class="btn btn-primary smoothscroll" style="margin-right: 5px" :style="[
+                  { background: themeColor },
+                  { borderColor: themeColor },
+                ]">{{ buttonSeeFeature }}</a>
+                <a href="#pricing-section" class="btn btn-outline-primary smoothscroll pricing"
+                  :style="[{ color: themeColor }, { borderColor: themeColor }]">{{ buttonPricing }}</a>
               </p>
             </div>
           </div>

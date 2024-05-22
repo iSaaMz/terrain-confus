c27988eb05661eed910b72395d0a778cec993910
iSaaMz <sami.assiakh@gmail.com>
2024-05-22 08:40:22 +0200

    update data item

--- src/data/items.js
+++ src/data/items.js
@@ -1,12 +1,12 @@
 const themeColor = '#407BFF'
-const siteName = 'Launch'
+const siteName = 'Terrain confus'
 
 const contactInfo = {
-    address: '43 Raymouth Rd. Baltemoer, London 3910',
-    phone1: '+1(123)-456-7890',
-    phone2: '+1(123)-456-7890',
-    email: 'info@mydomain.com',
-    website: 'https://yourwebsite.com/'
+    address: '256 Rue du Faubourg Saint-Antoine, 75012 Paris, France',
+    phone1: '01 23 45 67 89',
+    phone2: '01 89 98 89 55',
+    email: 'terrain-confus@info.com',
+    website: 'https://terrain-confus-info.com/'
 }
 
 export { themeColor, siteName, contactInfo }
